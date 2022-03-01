3d786157636dc22f7b91accbbee8c89bc120f101
Yury Beltrikov <ybeltrikov@gmail.com>
2022-03-01 18:09:23 +0000

    Sanitised corp data and added 2 scripts

---
+++ New-CorpUKUser.ps1
@@ -0,0 +1,105 @@
+﻿<#
+.SYNOPSIS
+This is a script creating a new corporate user
+
+.DESCRIPTION
+Script to automate new user account creation. It will prompt for the new user full name, OU, Title and Extension and will create:
+- CorpAccount account with temporary password
+- homedir folder for the user and sets permissions
+- Alternative domain account for systems not supporting  and save the details to network share
+
+FURTHER DEVELOPMENT.
+- Error for users:
+    -- with spaces in the names: Paul van Dyk
+    -- too long names: 
+
+#>
+
+
+#Creating new Corp UK user
+
+# 1 - Get the user's info
+$FirstName = Read-Host "First name"
+$LastName = Read-Host "Last name"
+$Title = Read-Host "Title"
+$Extension = Read-Host "Extension"
+$Department = Read-Host "Department"
+$Company = "Company Name"
+
+$Office = "London"
+$NewUserDomain = "@corpdomain.local"
+
+$NewUserName = ($FirstName + "." + $LastName).ToLower()
+$NewUserEmail = $NewUserName + "@corpemail.com"
+$NewUserPrincipalName = $NewUserName + $NewUserDomain
+$NewUserDisplayName = $FirstName + " " + $LastName
+
+$TempPassword = 'SomeTempOne22'
+$SecureStringPassword = ConvertTo-SecureString -String $TempPassword -AsPlainText -Force
+
+# 2- Create AD accounts
+Write-Host "Creating New AD user $NewUserName"
+#Write-Verbose -Message "Creating New AD user: $NewUserName"
+
+$ParentOU = "OU=London,OU=parerntou,DC=corpdomain,DC=local"
+#List all OUs and ask where to put the account
+Get-ADOrganizationalUnit -SearchBase $ParentOU -SearchScope OneLevel -Filter * | Select-Object -ExpandProperty Name | Write-Host 
+Write-Host 
+$NewUserOU = Read-Host "Which OU? (from the above)"
+$TargetOU = "OU=$NewUserOU," + $ParentOU
+
+#create a new AD user in corp domain - New-ADUser cmdlet - 
+#https://docs.microsoft.com/en-us/powershell/module/addsadministration/new-aduser?view=win10-ps
+$NewUser = New-ADUser -UserPrincipalName $NewUserPrincipalName -SamAccountName $NewUserName -AccountPassword $SecureStringPassword -Name $NewUserDisplayName -GivenName $FirstName -Surname $LastName -DisplayName $NewUserDisplayName -Title $Title -OfficePhone $Extension -Path $TargetOU -Office $Office -Company $Company -Department $Department -EmailAddress $NewUserEmail -ChangePasswordAtLogon $true -PassThru -Confirm
+
+#create a new AD user in altdomain with random password - for spark and RocketChat
+
+#generate random pass https://devblogs.microsoft.com/scripting/generate-random-letters-with-powershell/
+$RandomPass = -join ((65..90) + (97..122) | Get-Random -Count 8 | % {[char]$_})
+$SpecialCharacter = -join ((33..63) | Get-Random -Count 1 | % {[char]$_})
+$RandomPass = $RandomPass + $SpecialCharacter
+
+#create the account
+$altdomainNewUserDomain = "@altdomain.local"
+$altdomainNewUserName = ($FirstNameLetter + $LastName).ToLower()
+$altdomainNewUserPrincipalName = $altdomainNewUserName + $altdomainNewUserDomain
+$altdomainNewUserDisplayName = $FirstName + " " + $LastName
+$altdomainOU = "OU=Users,OU=UK,OU=altdomain,DC=altdomain,DC=local"
+$altdomainServer = "dc05.altdomain.local"
+$NewaltdomainUser = New-ADUser -Server $altdomainServer -UserPrincipalName $altdomainNewUserPrincipalName -SamAccountName $altdomainNewUserName -AccountPassword $RandomSecureStringPassword -Name $altdomainNewUserDisplayName -GivenName $FirstName -Surname $LastName -DisplayName $altdomainNewUserDisplayName -Title $Title -OfficePhone $Extension -Path $altdomainOU -Office $Office -ChangePasswordAtLogon $true -Company $Company -Department $Department -EmailAddress $NewUserEmail -PasswordNeverExpires $true -CannotChangePassword $trueget- -PassThru -Confirm
+
+#save the user details to network share J:\I.T\Keys and IDs\altdomain logins
+$CredentialsFolder = "J:\I.T\Keys and IDs\altdomain logins\"
+$CredentialsFilename = $altdomainNewUserName + ".txt"
+$CredentialsFullFilename = $CredentialsFolder + $CredentialsFilename
+
+$altdomainNewUserName | Out-File -FilePath $CredentialsFullFilename
+$RandomPass | Out-File -FilePath $CredentialsFullFilename -Append
+
+# 3 - Add the altdomainuk and altdomain users to the default groups
+$DefaultUKGroups = 'CN=KAV_USB_London_deny,OU=KAV,OU=Security Group,DC=altdomainuk,DC=local', 'CN=Print_HPM775f_Colour,OU=Printserver,OU=Security Group,DC=altdomainuk,DC=local','CN=Map_J,OU=Security Group,DC=altdomainuk,DC=local','CN=Map_K,OU=Security Group,DC=altdomainuk,DC=local','CN=MAP_R,OU=Security Group,DC=altdomainuk,DC=local','CN=Print_HPM775f_Finance_Colour,OU=Printserver,OU=Security Group,DC=altdomainuk,DC=local','CN=Proxy_Users,OU=Security Group,DC=altdomainuk,DC=local'
+$DefaultUKGroups | Get-ADGroup | Add-ADGroupMember -Members $NewUser
+
+$DefaultaltdomainGroups = "CN=Access.APP.RocketChatMembers,OU=RocketChat,OU=Applications,OU=Access Groups,DC=altdomain,DC=local"
+$DefaultaltdomainGroups | Get-ADGroup -Server $altdomainServer | Add-ADGroupMember -Server $altdomainServer -Members $NewaltdomainUser
+
+# 4 - Create homedir and set permissions
+# https://blogs.msdn.microsoft.com/johan/2008/10/01/powershell-editing-permissions-on-a-file-or-folder/
+$HomeDirLocation = '\\path\to\Userhome'
+$HomeFolderName = $HomeDirLocation + "\" + $NewUserName
+$UserHomeFolder = New-Item -ItemType Directory -Path $HomeFolderName -Confirm
+$FolderAcl = Get-Acl $UserHomeFolder
+$AddNewPermissions = New-Object System.Security.AccessControl.FileSystemAccessRule($NewUser.SID,"Modify","ContainerInherit, ObjectInherit", "None", "Allow")
+$FolderAcl.AddAccessRule($AddNewPermissions)
+
+#the next line used to produce errors  
+#https://social.technet.microsoft.com/Forums/ie/en-US/295f196a-f213-4f66-a608-1743a6bda787/cannot-use-setacl-properly-despite-being-file-owner-and-being-a-member-of-administrators-group?forum=winserverpowershell
+#https://social.technet.microsoft.com/Forums/scriptcenter/en-US/87679d43-04d5-4894-b35b-f37a6f5558cb/solved-how-to-take-ownership-and-change-permissions-for-blocked-files-and-folders-in-powershell
+
+try {
+    Set-Acl -Path $UserHomeFolder -AclObject $FolderAcl    
+}
+catch {
+    Write-Host "Set-ACL error: permissions not set. Please set permissions on the homedir manually"
+}
+
---
+++ New-WorkflowUKFolder.ps1
@@ -0,0 +1,73 @@
+﻿<#
+.SYNOPSIS
+Script to create new folder in Z:\WorkflowUK and relevant R and RW groups
+
+.DESCRIPTION
+Script to automate new WorkflowUK folder creation. It will prompt for the folder name and owner
+
+#>
+
+
+#Ask for the folder name and owner
+
+$FolderName = Read-Host "Folder name: "
+$FolderOwner = Read-Host "Folder owner: "
+$FullFolderPath = "Z:\WorkflowUK\" + $FolderName
+
+$WorkflowUKPath = "Z:\WorkflowUK"
+
+#Create R and RW groups: Access and Rights
+#Group names: Access group is added to the Rights group, Rights group granted with Permissions
+#Access.FS.WorkFlowUK.<FolderName>.Full
+#Access.FS.WorkFlowUK.<FolderName>.Read
+#Rights.FS.WorkFlowUK.<FolderName>.Full
+#Rights.FS.WorkFlowUK.<FolderName>.Read
+
+#Set the Groups names
+$AccessGroupNameFull = "Access.FS.WorkFlowUK." + $FolderName + ".Full"
+$AccessGroupNameRead = "Access.FS.WorkFlowUK." + $FolderName + ".Read"
+$AccessGroupsADPath = "OU=File,OU=Access Groups,DC=corpdomain,DC=local"
+$RightsGroupNameFull = "Rights.FS.WorkFlowUK." + $FolderName + ".Full"
+$RightsGroupNameRead = "Rights.FS.WorkFlowUK." + $FolderName + ".Read"
+$RightsGroupsADPath = "OU=File,OU=Resources groups,DC=corpdomain,DC=local"
+$GroupsDescription = "Owner: " + $FolderOwner
+
+Write-Output "Creating security groups"
+
+#Create the groups and add Access groups to the Rights groups
+$AccessGroupFull= New-ADGroup -Name $AccessGroupNameFull -SamAccountName $AccessGroupNameFull -GroupCategory Security -GroupScope Global -DisplayName $AccessGroupNameFull -Path $AccessGroupsADPath -Description $GroupsDescription -PassThru -Confirm
+$AccessGroupRead= New-ADGroup -Name $AccessGroupNameRead -SamAccountName $AccessGroupNameRead -GroupCategory Security -GroupScope Global -DisplayName $AccessGroupNameRead -Path $AccessGroupsADPath -Description $GroupsDescription -PassThru -Confirm
+
+$RightGroupFull = New-ADGroup -Name $RightsGroupNameFull -SamAccountName $RightsGroupNameFull -GroupCategory Security -GroupScope DomainLocal -DisplayName $RightsGroupNameFull -Path $RightsGroupsADPath -Description $GroupsDescription -PassThru -Confirm
+$RightGroupRead = New-ADGroup -Name $RightsGroupNameRead -SamAccountName $RightsGroupNameRead -GroupCategory Security -GroupScope DomainLocal -DisplayName $RightsGroupNameRead -Path $RightsGroupsADPath -Description $GroupsDescription -PassThru -Confirm
+
+Add-ADGroupMember -Identity $RightGroupFull -Members $AccessGroupFull
+Add-ADGroupMember -Identity $RightGroupRead -Members $AccessGroupRead
+
+Write-Output "Security Groups created"
+
+#Create the folder and set permissions to the groups
+
+Write-Output "Creating folder and setting permissions"
+$CreatedFolder = New-Item -ItemType Directory -Path $FullFolderPath -Confirm
+
+$FolderAcl = Get-Acl $CreatedFolder
+$ReadAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($RightGroupRead.SID, "Read","ContainerInherit, ObjectInherit", "None", "Allow")
+$FullAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($RightGroupFull.SID, "Modify","ContainerInherit, ObjectInherit", "None", "Allow")
+$FolderAcl.AddAccessRule($ReadAccessRule)
+$FolderAcl.AddAccessRule($FullAccessRule)
+Set-Acl -Path $FullFolderPath -AclObject $FolderAcl
+
+#also apply special permissions to WorkflowUK
+#yet to be tested
+
+#$WorkflowUKFolder = get-item -Path $WorkflowUKPath
+#$WorkflowUKAcl = Get-Acl $WorkflowUKFolder
+#$SpecialPermissionsReadGroup =New-Object System.Security.AccessControl.FileSystemAccessRule($RightGroupRead.SID, "ListDirectory", "ReadAttributes", "ReadExtendedAttributes" ,"ContainerInherit, ObjectInherit", "None", "Allow")
+#$SpecialPermissionsFullGroup =New-Object System.Security.AccessControl.FileSystemAccessRule($RightGroupFull.SID,"ListDirectory", "ReadAttributes", "ReadExtendedAttributes","ContainerInherit, ObjectInherit", "None", "Allow")
+#$WorkflowUKAcl.AddAccessRule($SpecialPermissionsReadGroup)
+#$WorkflowUKAcl.AddAccessRule($SpecialPermissionsFullGroup)
+#Set-Acl -Path $WorkflowUKPath -AclObject $WorkflowUKAcl
+
+
+Write-Output "Folder created and permissions set"
