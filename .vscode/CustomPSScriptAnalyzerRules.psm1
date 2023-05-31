c4797bad64443b675720ac96cb1ee2f202d9f38e
ybeltrikov <ybeltrikov@gmail.com>
2023-05-31 18:27:39 +0100

    camelCase for variables

--- .vscode/CustomPSScriptAnalyzerRules.psm1
+++ .vscode/CustomPSScriptAnalyzerRules.psm1
@@ -79,3 +79,87 @@ Function Measure-PascalCase {
             }
         }
     }
+
+    Function Measure-CamelCase {
+        <#
+        .SYNOPSIS
+            The variables names should be in camelCase.
+    
+        .DESCRIPTION
+            Variables names should be in camelCase.
+            To fix a violation of this rule, please consider using camelCase for variable names.
+    
+        .EXAMPLE
+            Measure-CamelCase -ScriptBlockAst $ScriptBlockAst
+    
+        .INPUTS
+            [System.Management.Automation.Language.ScriptBlockAst]
+    
+        .OUTPUTS
+            [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
+    
+        .NOTES
+            https://msdn.microsoft.com/en-us/library/dd878270(v=vs.85).aspx
+            https://msdn.microsoft.com/en-us/library/ms229043(v=vs.110).aspx
+        #>
+    
+            [CmdletBinding()]
+            [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
+            Param
+            (
+                [Parameter(Mandatory = $True)]
+                [ValidateNotNullOrEmpty()]
+                [System.Management.Automation.Language.ScriptBlockAst]
+                $ScriptBlockAst
+            )
+    
+            Process {
+    
+                $Results = @()
+    
+                try {
+                    #region Define predicates to find ASTs.
+    
+                    [ScriptBlock]$Predicate = {
+                        Param ([System.Management.Automation.Language.Ast]$Ast)
+    
+                        [bool]$ReturnValue = $False
+                        If ($Ast -is [System.Management.Automation.Language.AssignmentStatementAst]) {
+    
+                            [System.Management.Automation.Language.AssignmentStatementAst]$VariableAst = $Ast
+                            If (($VariableAst.Left.VariablePath.UserPath -cnotmatch '^[a-z]+([A-Za-z0-9]+)+') -or ($VariableAst.Left.VariablePath.UserPath -cmatch '_')) {
+                                $ReturnValue = $True
+                            }
+                        }
+                        return $ReturnValue
+                    }
+                    #endregion
+    
+                    #region Finds ASTs that match the predicates.
+                    [System.Management.Automation.Language.Ast[]]$Violations = $ScriptBlockAst.FindAll($Predicate, $True)
+    
+                    If ($Violations.Count -ne 0) {
+    
+                        Foreach ($Violation in $Violations) {
+    
+                            $Result = [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+                                RuleName               = $PSCmdlet.MyInvocation.InvocationName
+                                Message                = "$((Get-Help $MyInvocation.MyCommand.Name).Description.Text)"
+                                Extent                 = $Violation.Extent
+                                "Severity"             = "Information"
+                                "SuggestedCorrections" = $null
+                            }
+                            $Results += $Result
+                        }
+                    }
+                    return $Results
+                    #endregion
+                }
+                catch {
+                    $PSCmdlet.ThrowTerminatingError($_)
+                }
+            }
+        }
+    
+
+Export-ModuleMember -Function Measure-*
--- Test-File.ps1
+++ Test-File.ps1
@@ -3,9 +3,20 @@
 #Using alias
 gps
 
-#variable not using PascalCase
+#variable not using camelCase
+$NowNow = Get-Date
+
 $nowNow = Get-Date
+
+$now = Get-Date
+
+$NOWNOW = Get-Date
+
+$now_date = Get-Date
+
 Write-Output $nowNow
+Write-Output $now_date
+Write-Output $now
 
 #parameter name not using PascalCase
 Get-Date -verbose
